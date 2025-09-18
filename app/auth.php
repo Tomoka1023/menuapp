c6edd780ebf0e6f659c5d6bf02ca828d1f659eab
トモカ <totto68911illustration@gmail.com>
2025-09-18 21:40:45 +0900

    Delete app directory

--- app/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-function current_user_id(): ?int {
-  return $_SESSION['user_id'] ?? null;
-}
-function require_login(): void {
-  if (!current_user_id()) {
-    header('Location: /login.php'); exit;
-  }
-}
